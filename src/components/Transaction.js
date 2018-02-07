9971aab8381d7aa77ea950f0214769894c124fbd
Tom Moir <trmoir9@gmail.com>
2018-02-07 15:49:32 +0000

    Transaction class renders the information as a table row
    tests green, now need to integrate into the BankAccount class

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,11 +4,31 @@ export default class Transaction extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      credit: props.credit
+      date    : new Date(),
+      credit  : props.credit,
+      debit   : props.debit,
+      balance : props.balance
     }
   }
 
   render() {
-    return null;
+    const state = this.state;
+    const dateOptions = {
+      year  : 'numeric',
+      month : '2-digit',
+      day   : '2-digit'
+    }
+
+    const credit = state.credit ? `£${state.credit.toFixed(2)}` : ''
+    const debit  = state.debit  ? `£${state.debit.toFixed(2)}`  : ''
+
+    return (
+      <tr>
+        <td>{state.date.toLocaleDateString('gregory', dateOptions)}</td>
+        <td>{credit}</td>
+        <td>{debit}</td>
+        <td>£{state.balance.toFixed(2)}</td>
+      </tr>
+    )
   }
 }
--- src/components/Transaction.test.js
+++ src/components/Transaction.test.js
@@ -3,14 +3,73 @@ import { shallow } from 'enzyme';
 import Transaction from './Transaction';
 
 describe('Transaction', () => {
+  const mockDate = new Date(2018, 1, 6);
+  let wrapper;
+  beforeEach(() => {
+    Date = class extends Date {
+      constructor() {
+        return mockDate
+      }
+    }
+
+    wrapper = shallow(
+      <Transaction
+        credit={10}
+        debit={10}
+        balance={15}
+      />
+    )
+  })
+
   describe('state', () => {
+    it('sets the date it was created', () => {
+      expect(wrapper.instance().state.date).toEqual(mockDate)
+    })
+
     it('sets the value of credit from Props', () => {
-      const wrapper = shallow(<Transaction credit={10} />);
       expect(wrapper.instance().state.credit).toEqual(10);
+    });
+
+    it('sets the value of debit from Props', () => {
+      expect(wrapper.instance().state.debit).toEqual(10);
+    });
+
+    it('sets the value of balance from Props', () => {
+      expect(wrapper.instance().state.balance).toEqual(15);
     });
   });
 
   describe('#render', () => {
+    it('displays the details of the transaction in a table', () => {
+      const wrapper = shallow(
+        <Transaction
+          credit={5}
+          debit={null}
+          balance={15}
+        />
+      )
+      const tableData = wrapper.find('td')
+      expect(tableData.length).toEqual(4);
+      expect(tableData.at(0).text()).toEqual("2018-02-06");
+      expect(tableData.at(1).text()).toEqual("£5.00");
+      expect(tableData.at(2).text()).toEqual("");
+      expect(tableData.at(3).text()).toEqual("£15.00");
+    })
 
+    it('displays the details of the transaction in a table', () => {
+      const wrapper = shallow(
+        <Transaction
+          credit={null}
+          debit={20}
+          balance={5}
+        />
+      )
+      const tableData = wrapper.find('td')
+      expect(tableData.length).toEqual(4);
+      expect(tableData.at(0).text()).toEqual("2018-02-06");
+      expect(tableData.at(1).text()).toEqual("");
+      expect(tableData.at(2).text()).toEqual("£20.00");
+      expect(tableData.at(3).text()).toEqual("£5.00");
+    })
   })
 })
